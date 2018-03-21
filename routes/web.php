35812a1d0ba2668dcb72480f9e28ad7114cfb02f
kibernetika <kibernetika@mail.ua>
2018-03-21 21:15:58 +0200

    1) Made Transactions GUI and logic for access to it

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    return redirect('/transactions');
+});
+
+Route::get('/transactions', function () {
+    return view('transactions');
+})->middleware('auth');
