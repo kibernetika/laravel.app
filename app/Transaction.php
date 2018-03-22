eb39f36bdfc3bfe78598174ebfc2841466f28c0c
kibernetika <kibernetika@mail.ua>
2018-03-22 06:40:32 +0200

    1) added API for adding a transaction 2) added API for adding a client

--- app/Transaction.php
+++ app/Transaction.php
@@ -8,13 +8,28 @@ class Transaction extends Model
 {
     protected $table = 'transaction';
 
+    protected $primaryKey = 'transactionId';
+
+    protected $appends = ['date'];
+
     protected $fillable = ['customerId', 'amount' ];
 
-    protected $primaryKey = 'transactionId';
+    protected $visible = ['transactionId', 'customerId', 'amount', 'date'];
+
 
     public function client()
     {
         return $this->belongsTo('App\Client', 'customerId');
     }
 
+    public function scopeOfClientsTransactions($query, $params)
+    {
+        $query->where('customerId', $params['customerId']);
+        return $query;
+    }
+
+    public function getDateAttribute()
+    {
+        return $this->attributes['created_at'];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Transaction;
+use App\Client;
 use Illuminate\Http\Request;
 
 /*
@@ -17,23 +19,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('customer', function() {
-    return  response()->json([
-        'message' => 'Create success'
-    ], 201);
+Route::post('customer/{name}/{cnp}', function($name, $cnp) {
+    $client = Client::create(['name' => $name, 'cnp' => $cnp]);
+    return response()->json([ 'customerId' => $client->id]);
 });
 
-Route::post('transaction', function() {
-    return  response()->json([
-        'message' => 'Create success'
-    ], 201);
+Route::post('transaction/{customerId}/{amount}', function($customerId, $amount) {
+    $transaction = Transaction::create(['customerId' => $customerId, 'amount' => $amount]);
+    return $transaction->toJson();
 });
 
-Route::get('transaction', function () {
-    return response(['transaction 1', 'transaction 2', 'transaction 3'],200);
+Route::get('transactions', function () {
+    return response(Transaction::all(),200);
 });
 
-Route::get('transaction/{customerId}/{amount}/{date}/{offset}/{limit}', function ($customerId) {
-    return response()->json(['productId' => "{customerId}"], 200);
+Route::get('transaction/{customerId}', function ($customerId) {
+    return response(Transaction::OfClientsTransactions(['customerId' => $customerId])->get(), 200);
 });
 
