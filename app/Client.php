54e9057f18b731c1e7240d197002e34d181cdf57
kibernetika <kibernetika@mail.ua>
2018-03-22 02:48:57 +0200

    1) added properties and relations in models 2) changed name app

--- app/Client.php
+++ app/Client.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+
+    protected $fillable = ['name', 'cnp'];
+
+    protected $dateFormat = 'U';
+
+    public function transaction()
+    {
+        return $this->hasMany('Transaction', 'customerId');
+    }
+
 }
--- app/Transactions.php
+++ app/Transactions.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transactions extends Model
 {
-    //
+
+    protected $fillable = ['customerId', 'amount' ];
+
+    protected $primaryKey = 'transactionId';
+
+    protected $dateFormat = 'U';
+
+    public function client()
+    {
+        return $this->belongsTo('App\Client', 'customerId');
+    }
+
 }
